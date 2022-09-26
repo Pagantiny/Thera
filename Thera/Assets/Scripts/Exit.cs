d0081e8f7a90eba953c7d0dc8d644704f9739ccf
EricPicciurro <114239264+EricPicciurro@users.noreply.github.com>
2022-09-26 12:36:04 +0200

    L

--- Thera/Assets/Scripts/Exit.cs
+++ Thera/Assets/Scripts/Exit.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
     Rigidbody body;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +15,14 @@ public class Exit : MonoBehaviour
     }
 
 
-    // Update is called once per frame
+    
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("You Win");
+            Debug.Log("Hai trovato l'uscita");
         }
+    
     }
+    
 }
